889af992ef5e42890d4ba7e477704f9b5352b763
mdmomenulislam1 <mdmomenulislam1@gmail.com>
2023-11-07 10:48:31 +0600

    jwt implementation

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 //Middle
-app.use(cors());
-app.use(express.json());
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.header('Access-Control-Allow-Credentials', 'true');
 
-//mdmomenulislam1
-//gHipeueyVkcrH9y7
+//     next();
+//   });
+
+
+app.use(cors({
+    origin: [
+        'http://localhost:5173'
+    ],
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.itfatud.mongodb.net/?retryWrites=true&w=majority`;
@@ -24,25 +39,76 @@ const client = new MongoClient(uri, {
     }
 });
 
+//Middle wares
+const logger = (req, res, next) =>{
+    console.log('Log: Info',req.method, req.url);
+    next();
+};
+
+const verifyToken = (req, res, next) =>{
+    const token = req?.cookies?.token;
+    // console.log('Token in Middleware.', token);
+    if (!token) {
+        return res.status(401).send({message : 'unauthorized access'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if (err) {
+            return res.status(401).send({message : 'unauthorized access'})
+        }
+        req.user = decoded ;
+        next();
+    })
+    // next();
+}
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        const foodCollection = client.db('foodItem').collection('foods');
 
+        app.post('/jwt', logger, async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            })
+                .send({ success: true });
+        });
+
+        app.post('/logout', async(req,res) =>{
+            const user =req.body;
+            res.clearCookie('token', {maxAge:0}).send({ success: true });
+        })
+
+
+
+
+        const foodCollection = client.db('foodItem').collection('foods');
 
         app.post("/foods", async (req, res) => {
             const food = req.body;
-            console.log("food", food);
+            // console.log("food", food);
             const result = await foodCollection.insertOne(food);
-            console.log(result);
+            // console.log(result);
             res.send(result);
         });
 
         app.get("/foods", async (req, res) => {
-            const result = await foodCollection.find().toArray();
-            console.log(result);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+
+            console.log('Pagination ',req.query);
+            const result = await foodCollection.find()
+            .skip(page*size)
+            .limit(size)
+            .toArray();
+            
+            // console.log(result);
             res.send(result);
         })
 
@@ -84,22 +150,31 @@ async function run() {
 
         const orderCollection = client.db('orderItem').collection('orders');
 
-        app.post("/orderedFoods", async (req, res) => {
+        app.post("/orderedfoods", async (req, res) => {
             const order = req.body;
-            console.log("order", order);
+            // console.log("order", order);
             const result = await orderCollection.insertOne(order);
-            console.log(result);
+            // console.log(result);
             res.send(result);
         });
 
 
-        app.get("/orderedFoods", async (req, res) => {
+        app.get("/orderedfoods", logger, verifyToken, async (req, res) => {
+            console.log(req.query.email);
+            console.log('CCCCCCCC',req.cookies);
+            // if (req.user.email !== req.query.email) {
+            //     return res.status(403).send({message: 'forbidden access'})
+            // }
+            let query = {};
+            if (req.query?.email) {
+                
+            }
             const result = await orderCollection.find().toArray();
-            console.log(result);
+            // console.log(result);
             res.send(result)
         })
 
-        app.delete('/orderedFoods/:id', async (req, res) => {
+        app.delete('/orderedfoods/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await orderCollection.deleteOne(query);
