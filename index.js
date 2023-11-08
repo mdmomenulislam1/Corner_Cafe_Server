acf30dd8cb09a3027d2e33fa8f3f87707406d466
mdmomenulislam1 <mdmomenulislam1@gmail.com>
2023-11-08 13:53:35 +0600

    Error Setupd

--- index.js
+++ index.js
@@ -7,17 +7,6 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-//Middle
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     res.header('Access-Control-Allow-Credentials', 'true');
-
-//     next();
-//   });
-
-
 app.use(cors({
     origin: [
         'http://localhost:5173'
@@ -30,7 +19,6 @@ app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.itfatud.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -39,7 +27,6 @@ const client = new MongoClient(uri, {
     }
 });
 
-//Middle wares
 const logger = (req, res, next) => {
     console.log('Log: Info', req.method, req.url);
     next();
@@ -47,7 +34,7 @@ const logger = (req, res, next) => {
 
 const verifyToken = (req, res, next) => {
     const token = req?.cookies?.token;
-    // console.log('Token in Middleware.', token);
+    
     if (!token) {
         return res.status(401).send({ message: 'unauthorized access' })
     }
@@ -58,13 +45,11 @@ const verifyToken = (req, res, next) => {
         req.user = decoded;
         next();
     })
-    // next();
 }
 
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
 
@@ -84,9 +69,6 @@ async function run() {
             const user = req.body;
             res.clearCookie('token', { maxAge: 0 }).send({ success: true });
         })
-
-
-
 
         const foodCollection = client.db('foodItem').collection('foods');
 
@@ -108,12 +90,8 @@ async function run() {
         })
 
         app.get("/foodsOrder", async (req, res) => {
-            // const page = parseInt(req.query.page);
-            // const size = parseInt(req.query.size);
             console.log('Pagination ', req.query);
             const result = await orderCollection.find()
-                // .skip(page * size)
-                // .limit(size)
                 .toArray();
             res.send(result);
         })
@@ -218,12 +196,12 @@ async function run() {
         app.get("/orderedFoods", logger, verifyToken, async (req, res) => {
             console.log(req.query.email);
             console.log('Cookies', req.cookies);
-            // if (req.user.email !== req.query.email) {
-            //     return res.status(403).send({message: 'forbidden access'})
-            // }
-            // let query = {};
-            // if (req.query?.email) {
-            // }
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({message: 'forbidden access'})
+            }
+            let query = {};
+            if (req.query?.email) {
+            }
             const result = await orderCollection.find().toArray();
             res.send(result)
         })
